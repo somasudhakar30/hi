24cdb8c70195ad53dd296548fc3be2b46f390c40
somasudhakar30 <somasudhakar30@gmail.com>
2024-07-12 00:03:25 +0530

    sai

---
+++ Day6.java
@@ -0,0 +1,20 @@
+Class Day6 
+{
+   public static void main(String[]args)
+   {
+     int i;
+     int j;
+     int n = 5;
+      
+     for(i=1;i<=n;i++)
+     {
+         for(j=1;j<=n;j++)
+         { 
+             System.out.print(i);  
+         }
+         System.out.println();
+     }
+  
+  
+    }
+}
